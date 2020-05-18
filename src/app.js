3cd4bf76c2fa84c631f047fdad48c7bc62b966a3
Mike Doubintchik <mike.doubintchik@gmail.com>
2020-05-18 19:59:33 -0400

    Added security headers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import compression from 'compression';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
+import frameguard from 'frameguard';
+import hsts from 'hsts';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
+import xssFilter from 'x-xss-protection';
 import routes from './server/routes';
 import swaggerDocument from './swagger.json';
 const debug = require('debug')('test:server');
-
 const app = express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // swagger documentation
@@ -15,6 +17,9 @@ app.use(compression()); // GZIP compression
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev')); // logger
+app.use(frameguard({ action: 'sameorigin' })); // x-frames header
+app.use(hsts({ maxAge: 5184000, includeSubDomains: true, preload: true })); // hsts header
+app.use(xssFilter()); // xss header
 
 // setup routes
 routes(app);
