a64d5045df33650e36ab01f9baf05dc4945c7d2c
Mike Doubintchik <mike.doubintchik@gmail.com>
2020-05-18 20:40:20 -0400

    Added morgan and cors

--- src/app.js
+++ src/app.js
@@ -11,12 +11,13 @@ import swaggerDocument from './swagger.json';
 const debug = require('debug')('test:server');
 const app = express();
 
+app.use(cors()); // cors
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // swagger documentation
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 })); // rate limiter
 app.use(compression()); // GZIP compression
 app.use(express.json({ limit: '300kb' })); // body-parser body size limit of 300kb
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev')); // logger
+app.use(morgan('tiny')); // logger
 app.use(frameguard({ action: 'sameorigin' })); // x-frames header
 app.use(hsts({ maxAge: 5184000, includeSubDomains: true, preload: true })); // hsts header
 app.use(xssFilter()); // xss header
